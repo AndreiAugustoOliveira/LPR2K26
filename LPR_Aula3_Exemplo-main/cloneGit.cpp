7a8c654af8c9646537c2d7d607a84cf346dbd25f
AndreiAugustoOliveira <andreifornalha@gmail.com>
2026-03-10 13:53:01 -0300

    Aula03/cloneGit.cpp Aula03/cloneGit.exe

--- LPR_Aula3_Exemplo-main/cloneGit.cpp
+++ LPR_Aula3_Exemplo-main/cloneGit.cpp
@@ -7,12 +7,12 @@ int main() {
     setlocale(LC_ALL, "pt_BR.utf8"); //Para usar acentuação
     cout << "\033[1;34m***************************************************\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
-    cout << "\033[1;32m*   \033[1;33m PARABÉNS 34DS 2K26!                    \033[1;32m          *\033[0m\n";
+    cout << "\033[1;32m*   \033[1;33m PARABÉNS 34DS!                     \033[1;32m          *\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
     cout << "\033[1;32m*   \033[1;36mVocê clonou um repositório público do Git     \033[1;32m*\033[0m\n";
     cout << "\033[1;32m*   \033[1;36mpara o seu repositório local!                 \033[1;32m*\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
     cout << "\033[1;34m***************************************************\033[0m\n";
-   // A alteração foi feita na linha 10, adicionei o "2K26"
-    return 0; 
+
+    return 0;
 }

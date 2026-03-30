ea57cb51a5f18c3b857715be479148d124bfc1b8
AndreiAugustoOliveira <andreifornalha@gmail.com>
2026-03-29 21:54:29 -0300

    Commit_Restante

---
+++ Aula05/Ex4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace LPR
+{
+ class Program
+  {
+   static void Main()
+    {
+      int num = 0;
+      int soma = 0;
+      int digito =0;
+
+        Console.Clear();
+        
+        Console.WriteLine("Digite um número:");
+        num = Convert.ToInt32(Console.ReadLine());
+        int quadrado = num * num;
+
+        while(quadrado > 0)
+            {
+                digito = quadrado % 10;
+                soma += digito;
+                quadrado /= 10;
+            }
+
+          Console.WriteLine("A soma dos dígitos do quadrado é: " + soma);
+
+    }
+  }
+}
---
+++ Aula05/Ex5.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace LPR
+{
+ class Program
+  {
+   static void Main()
+    {
+
+    Console.Clear();
+
+     int horas = 0;
+     int HorasTotais = 1000;
+     int meses = 0;
+     int semanas = 0;
+     int dias = 0;
+
+
+     Console.WriteLine("Jovem discípulo, você está prestes a trilhar o maior desafio da sua vida");
+     Console.WriteLine("Para se tornar um verdadeiro Jedi você deve treinar arduamente e ter um total de 1000 horas de treinamento");
+     Console.WriteLine("Quantas horas você treinará por dia? (você deve descansar aos finais de semana)");
+
+     horas = Convert.ToInt32(Console.ReadLine());
+     int HorasSemana = horas*5;
+     double HorasMes = HorasSemana*4.5;
+
+
+     while (HorasTotais != 0)
+      {
+        if (HorasTotais > HorasMes)
+        {
+          meses++;
+          HorasTotais = HorasTotais - Convert.ToInt32(HorasMes);
+        }
+
+        else if (HorasTotais > HorasSemana)
+        {
+          semanas++;
+          HorasTotais = HorasTotais - Convert.ToInt32(HorasSemana);
+        }
+
+        else
+        {
+          dias++;
+          HorasTotais = HorasTotais - horas;
+        }
+      }
+
+      Console.WriteLine($"Você precisará de {meses} meses, {semanas} semanas e {dias} dias para se tornar um verdadeiro Jedi");
+    }
+  }
+}
